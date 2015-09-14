e1b895022a517228b063e98ecee8aef07ff08a21
Alex Dowad <alexinbeijing@gmail.com>
2015-09-14 09:59:02 +0200

    Remove reference to Hamster from array_spec

--- spec/lib/immutable/core_ext/array_spec.rb
+++ spec/lib/immutable/core_ext/array_spec.rb
@@ -6,7 +6,7 @@ describe Array do
   describe "#to_list" do
     let(:to_list) { array.to_list }
 
-    it "returns an equivalent hamster list" do
+    it "returns an equivalent Immutable list" do
       expect(to_list).to eq(L["A", "B", "C"])
     end
   end
